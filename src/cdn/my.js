5b01cf538cc77d0d73fd334acaa398a26c225b3d
diyainfocare <diyainfocare@gmail.com>
2019-12-20 12:38:32 +0530

    3 files modified

---
+++ src/cdn/my.js
@@ -0,0 +1,7 @@
+$(function() {
+    alert('Hello, custom js');
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
